1ef9ea8307542965a3f13c60e860b5f05abdbd0e
Кирилл <k_kirill45@mail.ru>
2022-04-15 10:51:54 +0300

    Создал класс ExcelTable, добавил админку.

--- main.py
+++ main.py
@@ -8,37 +8,48 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 PATH_TO_EXCEL = os.getenv('PATH_TO_EXCEL')
-WORKBOOK = openpyxl.load_workbook(PATH_TO_EXCEL)
-SHEET = WORKBOOK['main']
+ADMINS = os.getenv('ADMINS').split(',')
 BUTTONS = ['Получить промокод', 'Получить баланс', 'Потратить']
-
 bot = telebot.TeleBot(TOKEN)
 
 
-def get_data_from_excel():
-    data = []
-    for row in SHEET['B2':f'C{SHEET.max_row}']:
-        data_from_fow = []
-        for cell in row:
-            data_from_fow.append(cell.value)
-        data.append(data_from_fow)
-    return data
+class ExcelTable:
+    def __init__(self):
+        self.workbook = openpyxl.load_workbook(PATH_TO_EXCEL)
+        self.sheet = self.workbook['main']
+        self.max_row = self.sheet.max_row
 
+    def get_data(self):
+        data = []
+        for row in self.sheet['B2':f'C{self.max_row}']:
+            data_from_fow = []
+            for cell in row:
+                data_from_fow.append(cell.value)
+            data.append(data_from_fow)
+        return data
 
-def get_id_from_excel():
-    data = []
-    for row in SHEET['A2':f'A{SHEET.max_row}']:
-        for cell in row:
-            data.append(cell.value)
-    return data
+    def get_id(self):
+        data = []
+        for row in self.sheet['A2':f'A{self.max_row}']:
+            for cell in row:
+                data.append(cell.value)
+        return data
+
+    def write_new_user(self, telegram_id, username):
+        self.sheet[f'A{self.max_row} + 1'] = telegram_id
+        self.sheet[f'B{self.max_row}'] = username
+        self.workbook.save(PATH_TO_EXCEL)
+
+    def reload_table(self):
+        self.__init__()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    if message.chat.id not in get_id_from_excel():
-        SHEET[f'A{SHEET.max_row + 1}'] = message.chat.id
-        SHEET[f'B{SHEET.max_row }'] = message.chat.username
-        WORKBOOK.save(PATH_TO_EXCEL)
+    if message.chat.id not in table.get_id():
+        table.write_new_user(
+            message.chat.id, message.chat.username
+        )
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     for button in BUTTONS:
         markup.add(telebot.types.KeyboardButton(button))
@@ -48,10 +59,18 @@ def start_message(message):
     )
 
 
+@bot.message_handler(commands=['reload'])
+def reload_table_from_message(message):
+    if str(message.chat.id) in ADMINS:
+        bot.send_message(
+            message.chat.id, 'Таблица перезагружена'
+        )
+
+
 @bot.message_handler(content_types='text')
 def message_reply(message):
     if message.text == 'Получить промокод':
-        pass
+        print(table.get_data())
     elif message.text == 'Получить баланс':
         pass
     elif message.text == 'Потратить':
@@ -62,4 +81,5 @@ def message_reply(message):
         )
 
 
+table = ExcelTable()
 bot.infinity_polling()
